27a5dd0dc584f51398fdbc126ec624f7c409c041
Marek Kaštil <m.kastil@seznam.cz>
2020-04-27 22:36:21 +0200

    Updated build and hero IDs

--- src/components/button.js
+++ src/components/button.js
@@ -40,7 +40,8 @@ function MarvelButton(props) {
         const superHeroNames = ['Batman', 'Spider-man', 'Black Panther', 'Iron Man', 'Aquaman', 'Batgirl', 'Superman', 'Captain America', 'Captain Marvel', 'Wonder Woman', 'Black Widow', 'Doctor Strange'];
         
         if(counter === 7){
-            const superHeroSelect = Math.floor(Math.random() * 732);
+            const superHeroIds = [30, 34, 35, 38, 60, 63, 69, 75, 95, 105, 106, 145, 149, 156, 162, 176, 185, 196, 201, 213, 222, 227, 233, 234, 238, 251, 263, 273, 274, 275, 280, 298, 299, 303, 306, 313, 322, 328, 332, 346, 356, 361, 367, 374, 391, 413, 414, 416, 423, 479, 489, 490, 491, 522, 527, 530, 558, 567, 620, 627, 638, 659, 687, 697, 708, 714, 720, 729];
+            const superHeroSelect = superHeroIds[Math.floor(Math.random() * superHeroIds.length)];
 
             fetch("https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/10222158177165164/"+superHeroSelect)
             .then(res => res.json())
